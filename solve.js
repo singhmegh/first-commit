cb666f77627a2eae53d2e3ec4161151ab293817c
megha singh <meghasingh1045@gmail.com>
2024-07-31 14:52:11 +0530

    solve

---
+++ solve.js
@@ -0,0 +1,16 @@
+let text = "";
+const array =["1","9","2","10","12","13","19"];
+array.forEach(myFunction);
+ 
+function myFunction(item, index){
+    text+= index + ": " + item + "<br>";
+console.log(item)
+}
+
+//anonymous function//
+arr = ["1","2","3", "4", "5", "6", "7", "8"];
+arr.forEach((element) => {
+  console.log(element);
+});
+
+//function//
